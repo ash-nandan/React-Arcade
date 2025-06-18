a0c761de82382a02a76e4b03dd8c6056512bf620
Joel Tewhare <joeltewhare@gmail.com>
2025-06-19 03:22:05 +1200

    Add react state elements and event handlers

--- client/components/ScattergoriesForm.tsx
+++ client/components/ScattergoriesForm.tsx
@@ -1,54 +1,46 @@
-function GameForm() {
+import { useState } from 'react'
+import sgLists from '../../data/sgLists'
+
+export default function GameForm() {
+  const [answer, setAnswer] = useState(
+    Object.keys(sgLists).reduce((acc, cat) => {
+      acc[cat] = ''
+      return acc
+    }),
+    //new learning: Object.keys used with .reduce lets you build a new object with the same keys but different values (ie cat1: '')
+  )
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log('Submitting:', answer)
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAnswer(event.target.value)
+    console.log('Text changed:', event.target.value)
+  }
+
   return (
     <div>
       <h3>Your chosen letter is P</h3>
       <div className="gameForm">
-        <form>
-          <div className="singleForm">
-            <label htmlFor="cat1">1. A boy&apos;s name </label>
-            <input id="cat1" />
-          </div>
-          <div className="singleForm">
-            <label htmlFor="cat2">2. Gifts </label>
-            <input id="cat2" />
-          </div>
-          <div className="singleForm">
-            <label htmlFor="cat3">3. An animal </label>
-            <input id="cat3" />
-          </div>
-          <div className="singleForm">
-            <label htmlFor="cat4">4. Things that are cold </label>
-            <input id="cat4" />
-          </div>
-          <div className="singleForm">
-            <label htmlFor="cat5">5. Colours </label>
-            <input id="cat5" />
-          </div>
-          <div className="singleForm">
-            <label htmlFor="cat6">6. Breakfast foods </label>
-            <input id="cat6" />
-          </div>
-          <div className="singleForm">
-            <label htmlFor="cat7">7. Things at the beach </label>
-            <input id="cat7" />
-          </div>
-          <div className="singleForm">
-            <label htmlFor="cat8">8. Movie titles </label>
-            <input id="cat8" />
-          </div>
-          <div className="singleForm">
-            <label htmlFor="cat9">9. Cities </label>
-            <input id="cat9" />
-          </div>
-          <div className="singleForm">
-            <label htmlFor="cat10">10. Musical instruments </label>
-            <input id="cat10" />
-          </div>
+        <form onSubmit={handleSubmit}>
+          {Object.entries(sgLists).map(([cat, value]) => (
+            <div key={cat} className="singleForm">
+              <label htmlFor={cat}>{value}</label>
+              <input
+                onChange={handleChange}
+                type="text"
+                name={cat}
+                id={cat}
+                value={answer}
+              />
+            </div>
+          ))}
+
           <button>Submit</button>
         </form>
       </div>
     </div>
   )
 }
-
-export default GameForm
