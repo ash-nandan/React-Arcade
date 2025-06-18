ae731654a542fd84a455deec9d65d0a731f6abf4
Joel Tewhare <joeltewhare@gmail.com>
2025-06-18 23:05:12 +1200

    Route Results component

---
+++ client/components/ScattergoriesResult.tsx
@@ -0,0 +1,20 @@
+function Results() {
+  return (
+    <>
+      <div>
+        <h2>The Results Are In!</h2>
+        <ul>
+          <li>Check your answers against other players</li>
+          <li>
+            Remember, you receive a point for every unique answer you have
+          </li>
+          <li>
+            Any matching answers with other players do NOT receive a point
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Results
--- client/router.tsx
+++ client/router.tsx
@@ -7,11 +7,13 @@ import {
 import App from './components/App'
 import Home from './components/Home'
 import Scattergories from './components/Scattergories'
+import Results from './components/ScattergoriesResult'
 
 const routes = createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<Home />} />
     <Route path="scattergories" element={<Scattergories />} />
+    <Route path="scattergories/results" element={<Results />} />
   </Route>,
 )
 
