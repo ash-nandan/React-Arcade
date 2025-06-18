9e221e49ab14742b73039dcab5872462a40613b9
Joel Tewhare <joeltewhare@gmail.com>
2025-06-18 18:53:10 +1200

    Add data folder

---
+++ data/sgLists.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    cat1: 'Clothing',
+    cat2: 'Gifts',
+    cat3: 'An animal',
+    cat4: 'Things that are cold',
+    cat5: 'Colours',
+    cat6: 'Breakfast foods',
+    cat7: 'Things at the beach',
+    cat8: 'Movie titles',
+    cat9: 'Cities',
+    cat10: 'Musical Instruments',
+  },
+]
