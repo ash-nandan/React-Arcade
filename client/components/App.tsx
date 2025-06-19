8a3674dc1698fb98998a31960b63d611c6109847
edwin-cajayon <cajayonedwinjr@gmail.com>
2025-06-19 22:12:25 +1200

    Remove duplicate titles from App component as they are now in Home component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router'
 function App() {
   return (
     <div>
-      <h1>React Arcade</h1>
-      <h1>Tiny Games, Big fun!</h1>
+      <h1></h1>
+      <h1></h1>
       <Outlet />
     </div>
   )
