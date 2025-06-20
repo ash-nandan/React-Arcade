ccd46faf6ea06a448153f54ac1c8a1fddaad6852
Joel Tewhare <joeltewhare@gmail.com>
2025-06-20 12:33:49 +1200

    Add testing setup folder

--- client/components/test/ScattergoriesForm.test.tsx
+++ client/components/test/ScattergoriesForm.test.tsx
@@ -1,11 +1,7 @@
+//@vitest-environment jsdom
 import { describe, it, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
+import '../test-setup/test-setup'
 import GameForm from '../ScattergoriesForm'
-
-describe('<GameForm />', () => {
-  it('has the correct structure', () => {
-    const { container } = render(<GameForm />)
-    expect(container).toMatchSnapshot()
-  })
-})
