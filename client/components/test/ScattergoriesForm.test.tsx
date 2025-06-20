e39bc266ff215c53acf4518b65b448cfaac6c4ab
Joel Tewhare <joeltewhare@gmail.com>
2025-06-20 13:48:05 +1200

    Pass inputs test

--- client/components/test/ScattergoriesForm.test.tsx
+++ client/components/test/ScattergoriesForm.test.tsx
@@ -2,21 +2,27 @@
 import { describe, it, expect } from 'vitest'
 import { screen, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { MemoryRouter } from 'react-router'
+import { MemoryRouter } from 'react-router-dom'
 
 import '../test-setup/test-setup'
 import GameForm from '../ScattergoriesForm'
 
 describe('<GameForm />', () => {
-  it('checks for letter prompt text', () => {
-    ;<MemoryRouter>
-      render(
-      <GameForm />)
-    </MemoryRouter>
+  it('checks for input forms', () => {
+    render(
+      <MemoryRouter>
+        <GameForm />
+      </MemoryRouter>,
+    )
+    //screen.debug()
 
-    const letterPrompt = screen.getByRole('heading', {
-      name: /your chosen letter is/i,
-    })
-    expect(letterPrompt).toBeInTheDocument()
+    const inputs = screen.getAllByRole('textbox')
+    expect(inputs).toHaveLength(10)
   })
 })
+
+//new learning: <MemoryRouter> is an in-memory version of a router you can wrap your component in for testing. I had to use this as I had the useNavigate hook as part of the GameForm component
+
+//new learning: its important to check if you are importing hooks from react-router or react-router-dom. Had real issues with getting tests to work and it was a simple errror like this (how I imported useNavigate in my component) that stopped my tests and had me confused for a long time trying to figure it out.
+
+//new learning: screen.debug() prints a snapshot of the current rendered HTML sturcture (DOM) to the test console to inspect if needed
