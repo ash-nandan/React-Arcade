9a4b111c3e1436fe2c1234ba8b7d91899754a097
Joel Tewhare <joeltewhare@gmail.com>
2025-06-20 13:13:40 +1200

    Add test and testing tools

--- client/components/test/ScattergoriesForm.test.tsx
+++ client/components/test/ScattergoriesForm.test.tsx
@@ -2,6 +2,21 @@
 import { describe, it, expect } from 'vitest'
 import { screen, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { MemoryRouter } from 'react-router'
 
 import '../test-setup/test-setup'
 import GameForm from '../ScattergoriesForm'
+
+describe('<GameForm />', () => {
+  it('checks for letter prompt text', () => {
+    ;<MemoryRouter>
+      render(
+      <GameForm />)
+    </MemoryRouter>
+
+    const letterPrompt = screen.getByRole('heading', {
+      name: /your chosen letter is/i,
+    })
+    expect(letterPrompt).toBeInTheDocument()
+  })
+})
