f037eeb3c28b5916b27a3dacd878f84ee827778b
Joel Tewhare <joeltewhare@gmail.com>
2025-06-22 11:26:51 +1200

    Add random letter state to extend play

--- client/components/ScattergoriesForm.tsx
+++ client/components/ScattergoriesForm.tsx
@@ -13,17 +13,24 @@ export default function GameForm() {
       obj[cat] = ''
       return obj
     }, {}),
-
-    //new learning: Object.keys used with .reduce lets you build a new object with the same keys but different values (ie cat1: '')
-    //new learning: <AnswerArray> is syntax for a type argument. Using it with useState is saying the state will hold an object thats like AnswerArray. Using it with reduce tells typescript I'm creating an object in the style of AnswerArray
   )
+  //new learning: Object.keys used with .reduce lets you build a new object with the same keys but different values (ie cat1: '')
+  //new learning: <AnswerArray> is syntax for a type argument. Using it with useState is saying the state will hold an object thats like AnswerArray. Using it with reduce tells typescript I'm creating an object in the style of AnswerArray
+  const [randomLetter] = useState(() => {
+    const letters = ['P', 'S', 'N']
+    return letters[Math.floor(Math.random() * 3)]
+    //Reminder: this is taking the indexes of the letters array and creating a whole number by randomly 'mathing' a number between 0-1, times it by 3 and round down
+  })
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     console.log('Submitting:', answer)
-    navigate('/scattergories/results', { state: answer })
+    navigate('/scattergories/results', {
+      state: { answer, letter: randomLetter },
+    })
     //new learning: useNavigate is used for navigation inside the code (like in a function) after something has happened, like a form submit we have here. It's placed in the component logic rather than in the return like a <Link />
     //new learning: { state: answer } is a navigation state that lets you send extra information along with the URL your navigating to, like an extra data package. We use in conjuction with react's useLocation function
+    //new learning: object shorthand - if your key and variable are the same, you can shortcut by writing once so {answer, letter: randomLetter} is the same as writing {answer: answer, ...}
   }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,7 +45,7 @@ export default function GameForm() {
 
   return (
     <div>
-      <h2 className="gameForm letter">Your chosen letter is P</h2>
+      <h2 className="gameForm letter">Your chosen letter is {randomLetter}</h2>
       <div>
         <form className="gameForm resultContainer" onSubmit={handleSubmit}>
           {Object.entries(sgLists[0]).map(([cat, value]) => (
