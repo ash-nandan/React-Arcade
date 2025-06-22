c1e1070678361ea1e1b3a9caf0763d403033473e
Joel Tewhare <joeltewhare@gmail.com>
2025-06-22 12:47:12 +1200

    Match answers displayed with letter chosen

--- client/components/ScattergoriesResult.tsx
+++ client/components/ScattergoriesResult.tsx
@@ -3,17 +3,18 @@ import sgLists from '../../data/sgLists'
 import sgPlayerAnswers from '../../data/sgPlayerAnswers'
 
 export default function Results() {
-  const playerTwo = sgPlayerAnswers[0]
-  const playerThree = sgPlayerAnswers[1]
-
   const location = useLocation()
-  const playerOne = location.state
+  const playerOne = location.state.answer
+  const letter = location.state.letter
   //new learning: useLocation is a react-router tool that gives you an object that contains everything about where the user is in your app. The important bit it contains for the results is the navigation state that was passed from the gameForm when submit is clicked. Kind of like a window into the router's memory. We reference the state key here and it brings in the answer info for us
+
+  const playerTwo = sgPlayerAnswers[letter][0]
+  const playerThree = sgPlayerAnswers[letter][1]
 
   return (
     <div>
+      <h2 className="gameForm info">The Results Are In!</h2>
       <div className="gameForm info">
-        <h2>The Results Are In!</h2>
         <li>Check your answers against other players</li>
         <li>Remember, you receive a point for every unique answer you have</li>
         <li>Any matching answers with other players do NOT receive a point</li>
